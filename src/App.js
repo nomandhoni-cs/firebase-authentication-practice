93ca12a570486fb55383a2c82ce4bd411cea7337
nomandhoni-cs <nomandhoni@yahoo.com>
2022-12-05 14:04:21 +0600

    Tested some firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Avatar, Button, Stack } from "@mui/material";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { auth, provider } from "./firebaseConfig";
 
-function App() {
+const App = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [displayName, setDisplayName] = React.useState("");
+  const [user, setUser] = React.useState({});
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+  // Sign in with email and password
+  const signInWithEmail = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        user.displayName = displayName;
+        setUser(user);
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  };
+
+  const userInfo = (
+    <Stack spacing={2} direction="row" justifyContent="center">
+      <Avatar
+        alt={user.displayName}
+        src={user.photoURL}
+        sx={{ width: 24, height: 24 }}
+      />
+      <p>{user.displayName}</p>
+    </Stack>
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.displayName && userInfo}
+      <Box
+        id="form"
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          required
+          type="text"
+          label="Full Name"
+          placeholder="Enter your full name"
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+        <TextField
+          required
+          type="email"
+          label="Email"
+          placeholder="yourname@example.com"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          required
+          type="password"
+          label="Password"
+          placeholder="Enter your password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button variant="contained" onClick={signInWithEmail}>
+          Sign Up
+        </Button>
+      </Box>
+      <Button
+        style={{ margin: "20px 0" }}
+        onClick={signInWithGoogle}
+        variant="contained"
+        color="secondary"
+      >
+        Login with Google
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth  } from "firebase/auth";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA9b2qZvVxuP_fy766yIHGeKfrLL1ngaMo",
+  authDomain: "o-auth-practice-8dc89.firebaseapp.com",
+  projectId: "o-auth-practice-8dc89",
+  storageBucket: "o-auth-practice-8dc89.appspot.com",
+  messagingSenderId: "1068487098091",
+  appId: "1:1068487098091:web:713da9f9d938d85a1248ae",
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider()
